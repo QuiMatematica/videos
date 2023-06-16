900e99c03d5f5397674b56f67e97afaf4137a612
QuiMatematica <claudio@quimatematica.it>
2023-06-16 19:11:57 +0200

    Bilancia

--- bilancia_1.py
+++ bilancia_1.py
@@ -22,62 +22,65 @@ class Scene(MovingCameraScene):
         pera = ImageMobject('img/frutta/pera.png').scale(.25)
         albicocca = ImageMobject('img/frutta/albicocca.png').scale(.15)
 
-        ananas_1_a = Group(
+        ananas_1_a = [
             ananas.copy().move_to(LEFT*5.8 + UP*1.5),
             ananas.copy().move_to(LEFT*4.6 + UP*1.5)
-        )
-        for f in ananas_1_a:
-            self.play(FadeIn(f, run_time=1/ananas_1_a.dim))
+        ]
+        self.mostra_frutta(ananas_1_a)
 
-        self.play(FadeIn(mela.copy().move_to(LEFT*6.3 + UP*mela_baseline)))
-        self.play(FadeIn(mela.copy().move_to(LEFT*5.4 + UP*mela_baseline)))
-        self.play(FadeIn(mela.copy().move_to(LEFT*4.5 + UP*mela_baseline)))
-        self.play(FadeIn(pera.copy().move_to(LEFT*5.9 + UP*0.45)))
-        self.play(FadeIn(pera.copy().move_to(LEFT*4.9 + UP*0.45)))
+        mele_1_a = [
+            mela.copy().move_to(LEFT * 6.3 + UP * mela_baseline),
+            mela.copy().move_to(LEFT * 5.4 + UP * mela_baseline),
+            mela.copy().move_to(LEFT * 4.5 + UP * mela_baseline)
+        ]
+        self.mostra_frutta(mele_1_a)
+
+        pere_1_a = [
+            pera.copy().move_to(LEFT * 5.9 + UP * 0.45),
+            pera.copy().move_to(LEFT * 4.9 + UP * 0.45)
+        ]
+        self.mostra_frutta(pere_1_a)
 
         ananas_1_b = Group(
             ananas.copy().move_to(LEFT*2.3 + UP*1.5),
             ananas.copy().move_to(LEFT*1.1 + UP*1.5)
         )
-        for f in ananas_1_b:
-            self.play(FadeIn(f, run_time=1/ananas_1_b.dim))
+        self.mostra_frutta(ananas_1_b)
 
-        self.play(FadeIn(mela.copy().move_to(LEFT*2.5 + UP*mela_baseline)))
-        self.play(FadeIn(mela.copy().move_to(LEFT*1.3 + UP*mela_baseline)))
+        mele_1_b = [
+            mela.copy().move_to(LEFT*2.5 + UP*mela_baseline),
+            mela.copy().move_to(LEFT*1.3 + UP*mela_baseline)
+        ]
+        self.mostra_frutta(mele_1_b)
         first = 2.9
         shift = 0.35
-        self.play(FadeIn(albicocca.copy().move_to(LEFT*(first + shift*0) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(LEFT*(first - shift*1) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(LEFT*(first - shift*2) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(LEFT*(first - shift*3) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(LEFT*(first - shift*4) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(LEFT*(first - shift*5) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(LEFT*(first - shift*6) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(LEFT*(first - shift*7) + UP*0.16)))
+        albicocche_1_b = []
+        for _i in range(8):
+            albicocche_1_b.append(albicocca.copy().move_to(LEFT*(first - shift*_i) + UP*0.16))
+        self.mostra_frutta(albicocche_1_b)
 
-        self.play(FadeIn(ananas.copy().move_to(RIGHT*1.1 + UP*1.5)))
-        self.play(FadeIn(ananas.copy().move_to(RIGHT*2.3 + UP*1.5)))
-        self.play(FadeIn(mela.copy().move_to(RIGHT*0.8 + UP*mela_baseline)))
-        self.play(FadeIn(mela.copy().move_to(RIGHT*2.1 + UP*mela_baseline)))
-        self.play(FadeIn(pera.copy().move_to(RIGHT*0.7 + UP*0.45)))
-        self.play(FadeIn(pera.copy().move_to(RIGHT*1.65 + UP*0.45)))
-        self.play(FadeIn(pera.copy().move_to(RIGHT*2.6 + UP*0.45)))
+        ananas_2_a = [ananas.copy().move_to(RIGHT*1.1 + UP*1.5),
+                      ananas.copy().move_to(RIGHT*2.3 + UP*1.5)]
+        self.mostra_frutta(ananas_2_a)
+        mele_2_a = [mela.copy().move_to(RIGHT*0.8 + UP*mela_baseline),
+                    mela.copy().move_to(RIGHT*2.1 + UP*mela_baseline)]
+        self.mostra_frutta(mele_2_a)
+        pere_2_a = [pera.copy().move_to(RIGHT*0.7 + UP*0.45),
+                    pera.copy().move_to(RIGHT*1.65 + UP*0.45),
+                    pera.copy().move_to(RIGHT*2.6 + UP*0.45)]
+        self.mostra_frutta(pere_2_a)
 
-        self.play(FadeIn(ananas.copy().move_to(RIGHT*4.7 + UP*1.5)))
-        self.play(FadeIn(ananas.copy().move_to(RIGHT*5.9 + UP*1.5)))
-        self.play(FadeIn(mela.copy().move_to(RIGHT*5.1 + UP*mela_baseline)))
+        ananas_2_b = [ananas.copy().move_to(RIGHT*4.7 + UP*1.5),
+                      ananas.copy().move_to(RIGHT*5.9 + UP*1.5)]
+        self.mostra_frutta(ananas_2_b)
+        mele_2_b = [mela.copy().move_to(RIGHT*5.1 + UP*mela_baseline)]
+        self.mostra_frutta(mele_2_b)
         first = 4.2
         shift = 0.25
-        self.play(FadeIn(albicocca.copy().move_to(RIGHT*(first + shift*0) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(RIGHT*(first + shift*1) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(RIGHT*(first + shift*2) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(RIGHT*(first + shift*3) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(RIGHT*(first + shift*4) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(RIGHT*(first + shift*5) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(RIGHT*(first + shift*6) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(RIGHT*(first + shift*7) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(RIGHT*(first + shift*8) + UP*0.16)))
-        self.play(FadeIn(albicocca.copy().move_to(RIGHT*(first + shift*9) + UP*0.16)))
+        albicocche_2_b = []
+        for _i in range(10):
+            albicocche_2_b.append(albicocca.copy().move_to(RIGHT*(first + shift*_i) + UP*0.16))
+        self.mostra_frutta(albicocche_2_b)
 
         self.wait(delay)
         self.next_section()
@@ -86,5 +89,38 @@ class Scene(MovingCameraScene):
         self.wait(delay)
         self.next_section()
 
+        self.play(FadeOut(mele_1_a[0]), FadeOut(mele_1_a[2]), *[FadeOut(f) for f in mele_1_b])
+        self.wait(delay)
+        self.next_section()
+
+        self.play(*[FadeOut(f) for f in ananas_2_a], *[FadeOut(f) for f in ananas_2_b])
+        self.wait(delay)
+        self.next_section()
+
+        self.play(FadeOut(mele_2_a[0]), FadeOut(mele_2_b[0]))
+        self.wait(delay)
+        self.next_section()
+
+        self.play(FadeOut(bilancia_sinistra))
+        self.wait(delay)
+        self.next_section()
+
+        uguale = MathTex("=").scale(2).move_to(LEFT * 3.85 + UP * 0.2)
+        self.play(Write(uguale))
+        self.wait(delay)
+        self.next_section()
+
+        self.play(FadeOut(mele_2_a[1]), FadeOut(pere_2_a[0]), FadeOut(pere_2_a[2]),
+                  *[FadeOut(f) for f in albicocche_2_b[:4]],
+                  *[FadeOut(f) for f in albicocche_2_b[6:]])
+        self.wait(delay)
+        self.next_section()
+
+        self.play(self.camera.frame.animate.move_to(bilancia_destra.get_center() + UP).scale(.5))
+
         self.wait(30)
+
+    def mostra_frutta(self, lista):
+        for f in lista:
+            self.play(FadeIn(f, run_time=1 / len(lista)))
 
